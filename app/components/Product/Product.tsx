ee00c3a48a93ea6db8b1825f99b9baeca528c154
Richo Febrian <richofal25@gmail.com>
2025-01-15 21:34:44 +0700

    Fixing Product Section

--- app/components/Product/Product.tsx
+++ app/components/Product/Product.tsx
@@ -23,76 +23,19 @@ function Product() {
   }, []);
 
   return (
-    <div className="bg-black text-white flex items-center justify-center py-16 px-8">
-      <div className="max-w-7xl mx-auto bg-black p-6 rounded-lg shadow-lg">
-        <div
-          className={`${
-            isLargeScreen
-              ? "grid grid-cols-[1fr,1.5fr] gap-8"
-              : "flex flex-col items-center"
-          }`}
-        >
+    <>
+      <div className="bg-black text-white flex items-center justify-center py-16 px-8">
+        <div className="max-w-7xl mx-auto bg-black p-6 rounded-lg">
           <div
             className={`${
-              isLargeScreen ? "flex justify-center items-center" : ""
+              isLargeScreen
+                ? "grid grid-cols-[1fr,1.5fr] gap-8"
+                : "flex flex-col items-center"
             }`}
           >
-            <div className="w-64">
-              <img
-                src="product.svg"
-                alt="Wooden bowl and utensils"
-                className={`rounded-full object-cover w-full h-full ${
-                  isLargeScreen ? "" : "hidden"
-                }`}
-              />
-            </div>
-          </div>
-          <div className={`${isLargeScreen ? "text-left" : "text-center"}`}>
-            <h2
-              className={`text-[#909298] ${
-                isLargeScreen ? "text-lg" : "text-md"
-              } font-bold mb-3`}
-            >
-              Produk
-            </h2>
-            <h1
-              className={`font-bold ${
-                isLargeScreen ? "mb-5 text-2xl" : "mb-7 mt-5 text-xl"
-              }`}
-            >
-              Harga Jasa Pembuatan Lukisan dan Batik (Lukis Bakar)
-            </h1>
-            <hr
-              className={`border-t border-[#A6795B] mb-4 ${
-                isLargeScreen ? "w-[20%]" : "w-[50%] mx-auto"
-              }`}
-            />
-            <p
-              className={`text-white mb-6 ${
-                isLargeScreen
-                  ? "text-left w-full"
-                  : "text-center w-full mx-auto py-5 text-md"
-              }`}
-              style={{ lineHeight: "25px" }}
-            >
-              Harga layanan seni lukis bakar yang kami tawarkan sangat bersaing.
-              Biaya tergantung pada, tingkat detail, dimensi desain, serta
-              tingkat kesulitan. Pigura jati dan alas kayu sengon. Harga mulai
-              dari 169 rb. Pemesanan dalam jumlah besar berhak mendapatkan
-              diskon khusus.
-            </p>
-            <button
-              className={`bg-white text-black font-medium ${
-                isLargeScreen ? "w-full" : ""
-              } py-2 px-4 rounded-sm`}
-            >
-              Pesan Sekarang
-            </button>
             <div
               className={`${
-                isLargeScreen
-                  ? ""
-                  : "mt-14 mx-auto flex justify-center items-center"
+                isLargeScreen ? "flex justify-center items-center" : ""
               }`}
             >
               <div className="w-64">
@@ -100,7 +43,146 @@ function Product() {
                   src="product.svg"
                   alt="Wooden bowl and utensils"
                   className={`rounded-full object-cover w-full h-full ${
-                    isLargeScreen ? "hidden" : ""
+                    isLargeScreen ? "" : "hidden"
+                  }`}
+                />
+              </div>
+            </div>
+            <div className={`${isLargeScreen ? "text-left" : "text-center"}`}>
+              <h2
+                className={`text-[#909298] ${
+                  isLargeScreen ? "text-lg" : "text-md"
+                } font-bold mb-3`}
+              >
+                Produk
+              </h2>
+              <h1
+                className={`font-bold ${
+                  isLargeScreen ? "mb-5 text-2xl" : "mb-7 mt-5 text-xl"
+                }`}
+              >
+                Harga Jasa Pembuatan Lukisan dan Batik (Lukis Bakar)
+              </h1>
+              <hr
+                className={`border-t border-[#A6795B] mb-4 ${
+                  isLargeScreen ? "w-[20%]" : "w-[50%] mx-auto"
+                }`}
+              />
+              <p
+                className={`text-white mb-6 ${
+                  isLargeScreen
+                    ? "text-left w-full"
+                    : "text-center w-full mx-auto py-5 text-md"
+                }`}
+                style={{ lineHeight: "25px" }}
+              >
+                Harga layanan seni lukis bakar yang kami tawarkan sangat
+                bersaing. Biaya tergantung pada, tingkat detail, dimensi desain,
+                serta tingkat kesulitan. Pigura jati dan alas kayu sengon. Harga
+                mulai dari 169 rb. Pemesanan dalam jumlah besar berhak
+                mendapatkan diskon khusus.
+              </p>
+              <button
+                className={`bg-white text-black font-medium ${
+                  isLargeScreen ? "w-full" : ""
+                } py-2 px-4 rounded-sm`}
+              >
+                Pesan Sekarang
+              </button>
+              <div
+                className={`${
+                  isLargeScreen
+                    ? ""
+                    : "mt-14 mx-auto flex justify-center items-center"
+                }`}
+              >
+                <div className="w-64">
+                  <img
+                    src="product.svg"
+                    alt="Wooden bowl and utensils"
+                    className={`rounded-full object-cover w-full h-full ${
+                      isLargeScreen ? "hidden" : ""
+                    }`}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-white text-black flex items-center justify-center py-16 px-8">
+        <div className="max-w-7xl mx-auto bg-white p-2 rounded-lg">
+          <div
+            className={`${
+              isLargeScreen
+                ? "grid grid-cols-[1.5fr,1fr] gap-8"
+                : "flex flex-col items-center"
+            }`}
+          >
+            <div
+              className={`${
+                isLargeScreen ? "text-left my-auto" : "text-center"
+              }`}
+            >
+              <div
+                className={`${
+                  isLargeScreen
+                    ? ""
+                    : "mb-16 mx-auto flex justify-center items-center"
+                }`}
+              >
+                <div className="w-64">
+                  <img
+                    src="pengiriman.svg"
+                    alt="Wooden bowl and utensils"
+                    className={`rounded-full object-cover w-full h-full ${
+                      isLargeScreen ? "hidden" : ""
+                    }`}
+                  />
+                </div>
+              </div>
+              <h1
+                className={`font-bold ${
+                  isLargeScreen ? "mb-5 text-2xl" : "mb-7 mt-5 text-xl"
+                }`}
+              >
+                Pengiriman Domestik
+              </h1>
+              <hr
+                className={`border-t border-[#A6795B] mb-4 ${
+                  isLargeScreen ? "w-[20%]" : "w-[50%] mx-auto"
+                }`}
+              />
+              <p
+                className={`text-black mb-6 ${
+                  isLargeScreen
+                    ? "text-left w-full"
+                    : "text-center w-full mx-auto py-5 text-md"
+                }`}
+                style={{ lineHeight: "25px" }}
+              >
+                Untuk memastikan karya seni lukis bakar Anda sampai dengan aman,
+                kami mengemasnya dengan rapi dan hati-hati. Produk dilapisi
+                dengan bahan pelindung seperti kertas atau foam, lalu dimasukkan
+                ke dalam dus yang kokoh. Berbagai layanan pengiriman terpercaya,
+                seperti J&T, JNE, Grab. Anda dapat memilih opsi pengiriman
+                sesuai kebutuhan, memastikan produk tiba tepat waktu dengan
+                kondisi yang sempurna. Semua proses pengiriman dirancang untuk
+                menjaga keamanan dan kualitas karya seni Anda.
+              </p>
+            </div>
+            <div
+              className={`${
+                isLargeScreen ? "flex justify-center items-center" : ""
+              }`}
+            >
+              <div className="w-64">
+                <img
+                  src="pengiriman.svg"
+                  alt="Wooden bowl and utensils"
+                  className={`rounded-full object-cover w-full h-full ${
+                    isLargeScreen ? "" : "hidden"
                   }`}
                 />
               </div>
@@ -108,7 +190,87 @@ function Product() {
           </div>
         </div>
       </div>
-    </div>
+
+      <div className="bg-white text-black flex items-center justify-center pb-14 px-8">
+        <div className="max-w-7xl mx-auto bg-white p-2 rounded-lg">
+          <div
+            className={`${
+              isLargeScreen
+                ? "grid grid-cols-[1fr,1.5fr] gap-8"
+                : "flex flex-col items-center"
+            }`}
+          >
+            <div
+              className={`${
+                isLargeScreen ? "flex justify-center items-center" : ""
+              }`}
+            >
+              <div className="w-64">
+                <img
+                  src="garansi.svg"
+                  alt="Wooden bowl and utensils"
+                  className={`rounded-full object-cover w-full h-full ${
+                    isLargeScreen ? "" : "hidden"
+                  }`}
+                />
+              </div>
+            </div>
+            <div
+              className={`${
+                isLargeScreen ? "text-left my-auto" : "text-center"
+              }`}
+            >
+              <div
+                className={`${
+                  isLargeScreen
+                    ? ""
+                    : "mb-16 mx-auto flex justify-center items-center"
+                }`}
+              >
+                <div className="w-64">
+                  <img
+                    src="garansi.svg"
+                    alt="Wooden bowl and utensils"
+                    className={`rounded-full object-cover w-full h-full ${
+                      isLargeScreen ? "hidden" : ""
+                    }`}
+                  />
+                </div>
+              </div>
+              <h1
+                className={`font-bold ${
+                  isLargeScreen ? "mb-5 text-2xl" : "mb-7 mt-5 text-xl"
+                }`}
+              >
+                Garansi
+              </h1>
+              <hr
+                className={`border-t border-[#A6795B] mb-4 ${
+                  isLargeScreen ? "w-[20%]" : "w-[50%] mx-auto"
+                }`}
+              />
+              <p
+                className={`text-black mb-6 ${
+                  isLargeScreen
+                    ? "text-left w-full"
+                    : "text-center w-full mx-auto py-5 text-md"
+                }`}
+                style={{ lineHeight: "25px" }}
+              >
+                Garansi kami memberikan jaminan  untuk setiap produk yang kami
+                hasilkan. Kami berdedikasi untuk memberikan kualitas terbaik,
+                dan jika ada kerusakan atau ketidaksesuaian dengan standar yang
+                dijanjikan, kami akan memperbaiki atau mengganti produk tanpa
+                biaya tambahan.
+              </p>
+              <p className="opacity-80 italic">
+                NB : Garansi ini berlaku dengan kebijakan pelayanan
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
